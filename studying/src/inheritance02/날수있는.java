cd8e42ce775b5d921e3a56eb1d4209e54b9f29e3
김동준 <118492753+rigel9327@users.noreply.github.com>
2023-04-06 16:16:49 +0900

    Delete studying directory

--- studying/src/inheritance02/날수있는.java
+++
@@ -1,5 +0,0 @@
-package inheritance02;
-
-public interface 날수있는 {
-    void fly();
-}
--- studying/src/inheritance02/헤엄칠수있는.java
+++
@@ -1,7 +0,0 @@
-package inheritance02;
-
-import inheritance01.포유류;
-
-public interface 헤엄칠수있는 {
-    void swim();
-}
